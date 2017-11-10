1a3294af3d18bfa4e9c8c3e99cb7e7aa43c76cea
truongvandung <truongdung.vd@gmail.com>
2017-11-10 14:15:25 +0700

    a
    a

--- su_dynamic_listview_odoo_v10_ok/__manifest__.py
+++ su_dynamic_listview_odoo_v10_ok/__manifest__.py
@@ -17,7 +17,7 @@
         'static/src/xml/base.xml',
     ],
     'images': ['images/main_screen.jpg'],
-    'price': 99,
+    'price': 250,
     'currency': 'EUR',
     'installable': True,
     'auto_install': False,
